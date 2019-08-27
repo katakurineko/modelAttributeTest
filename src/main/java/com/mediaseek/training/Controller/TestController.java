46b9ed77417578f8f9b493fd78932a7fc23c0f0e
ryunosuke_kotaki <ryunosuke.kotaki@mediaseek.co.jp>
2019-08-27 12:03:26 +0900

    urlからパラメータを受け取るように変更

--- src/main/java/com/mediaseek/training/Controller/TestController.java
+++ src/main/java/com/mediaseek/training/Controller/TestController.java
@@ -3,28 +3,17 @@ package com.mediaseek.training.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import com.mediaseek.training.Entity.Hello;
 
 @Controller
 public class TestController {
-//  @ModelAttribute
-//  public Hello getMenu() {
-//    Hello h = new Hello();
-//    h.setHello("hello!");
-//    return h;
-//  }
 
-  @GetMapping("/")
-  public String get(@ModelAttribute("hello") Hello h) {
+  @GetMapping("/{message}")
+  public String get(@ModelAttribute("hello") Hello h,@PathVariable String message) {
     h.setPageName("test1");
-    h.setHello("hello!");
+    h.setHello(message);
     return "test";
-  }
-  
-  @GetMapping("/2")
-  public String get2(@ModelAttribute("hello") Hello h) {
-    h.setHello("hellohello!");
-    return "test2";
   }
 
 }
