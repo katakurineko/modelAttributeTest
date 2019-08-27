26e5a5c439912b85754f2703e5869fd6979ba08f
ryunosuke_kotaki <ryunosuke.kotaki@mediaseek.co.jp>
2019-08-27 11:23:16 +0900

    Helloエンティティを使用するように変更

--- src/main/java/com/mediaseek/training/Controller/TestController.java
+++ src/main/java/com/mediaseek/training/Controller/TestController.java
@@ -3,13 +3,17 @@ package com.mediaseek.training.Controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import com.mediaseek.training.Entity.Hello;
 
 @Controller
 public class TestController {
 
   @GetMapping("/")
   public String get(Model model) {
-    model.addAttribute("hello", "hello!");
+    Hello h = new Hello();
+    h.setHello("hello!");
+    model.addAttribute("hello",h);
     return "test";
   }
 
---
+++ src/main/java/com/mediaseek/training/Entity/Hello.java
@@ -0,0 +1,14 @@
+package com.mediaseek.training.Entity;
+
+public class Hello {
+  private String hello;
+
+  public String getHello() {
+    return hello;
+  }
+
+  public void setHello(String hello) {
+    this.hello = hello;
+  }
+
+}
